0d4cf1e6b49a7621c8ee56e8bf1849f6cdc39d20
Tim Nguyen <nt1m@users.noreply.github.com>
2015-01-14 22:40:10 +0100

    More indications

--- calculator.js
+++ calculator.js
@@ -1,15 +1,16 @@
-var input = /* Use document.querySelector("element") to target the input */;
+var input = /* Use document.querySelector("element's css selector") to target the input */;
 var pushCharacter = function(character) {
     /* This will push the character to the input.value */
     input.value = input.value + /* Guess what */;
+}
+var buttons = document.querySelectorAll(".button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = pushCharacter(buttons[i].innerHTML);
 }
 
 var calculate = function() {
     /* Set input.value to eval(input.value).toString() */
 }
+var equalbutton = /* Same for input, but instead with .button-equals */;
+equalbutton.onclick = calculate();
 
-var buttons = document.querySelectorAll(".button");
-
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = pushCharacter(buttons[i].innerHTML);
-}
