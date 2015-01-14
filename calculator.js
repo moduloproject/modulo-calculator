d93b6ce7dd526c66fa1fa26efc176c60bda7aa83
Tim Nguyen <nt1m@users.noreply.github.com>
2015-01-14 22:37:23 +0100

    Indications for the calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,12 @@
-var pushCharacter = function(character) {}
+var input = /* Use document.querySelector("element") to target the input */;
+var pushCharacter = function(character) {
+    /* This will push the character to the input.value */
+    input.value = input.value + /* Guess what */;
+}
 
-var calculate = function() {}
+var calculate = function() {
+    /* Set input.value to eval(input.value).toString() */
+}
 
 var buttons = document.querySelectorAll(".button");
 
