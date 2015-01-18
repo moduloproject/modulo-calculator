d9b9fa3607b30725994847aa10ae292f99c0f5b1
mufc4martin <mufc4martin@gmail.com>
2015-01-18 20:29:10 +0100

    change function declaration

--- calculator.js
+++ calculator.js
@@ -1,29 +1,3 @@
-window.onload = function() {
-    pushCharacter = function(character) {
-        var input = document.querySelector("#display");
-        input.value = input.value + character;
-    };
-
-    var buttons = document.querySelectorAll(".button");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = function() { pushCharacter(buttons[i].innerHTML); }
-    };
-
-    calculate = function() {
-        var input = document.querySelector("#display");
-        input.value = eval(input.value).toString();
-    };
-    var equalbutton = document.querySelector(".button-equals");
-    equalbutton.onclick = calculate;
-
-    clear = function() {
-        var input = document.querySelector("#display");
-        input.value = "";
-    };
-    var clearbutton = document.querySelector(".clearbutton");
-    clearbutton.onclick = clear;
-};
-
 function window.onload() {
     function pushCharacter(character) {
         var input = document.querySelector("#display");
