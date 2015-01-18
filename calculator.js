2b586a53a9aaa24db681b65cb121a05363635208
Tim Nguyen <nt1m@users.noreply.github.com>
2015-01-18 20:33:33 +0100

    Fixed js

--- calculator.js
+++ calculator.js
@@ -1,25 +1,25 @@
-function window.onload() {
-    function pushCharacter(character) {
-        var input = document.querySelector("#display");
-        input.value = input.value + character;
-    };
+function pushCharacter(character) {
+    var input = document.querySelector("#display");
+    input.value = input.value + character;
+};
+function calculate() {
+    var input = document.querySelector("#display");
+    input.value = eval(input.value).toString();
+};
+function clear() {
+    var input = document.querySelector("#display");
+    input.value = "";
+};
 
+window.onload = function() {
     var buttons = document.querySelectorAll(".button");
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].onclick = function() { pushCharacter(buttons[i].innerHTML); }
     };
 
-    function calculate() {
-        var input = document.querySelector("#display");
-        input.value = eval(input.value).toString();
-    };
     var equalbutton = document.querySelector(".button-equals");
     equalbutton.onclick = calculate;
 
-    function clear() {
-        var input = document.querySelector("#display");
-        input.value = "";
-    };
     var clearbutton = document.querySelector(".clearbutton");
     clearbutton.onclick = clear;
-};
+}
